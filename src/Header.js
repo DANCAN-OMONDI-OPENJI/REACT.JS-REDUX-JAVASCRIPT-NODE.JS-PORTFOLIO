4591ab009a5dd599d87137b73727ec2392a463b7
DANCAN OMONDI OPENJI <palmargenesis33@gmail.com>
2023-02-08 00:13:23 +0300

    Modified Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="header">
       <p>DANCAN OPENJI</p>
       <ul className="header-links">
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Blog</li>
-        <li>Contact</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="contact">Contact</Link>
+        </li>
       </ul>
     </div>
   );
