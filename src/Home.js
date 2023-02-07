e621815c0219e7cb687eedfb8bbb4f3f4ca9d023
DANCAN OMONDI OPENJI <palmargenesis33@gmail.com>
2023-02-07 20:11:26 +0300

    Added Home.js

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import "./index.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
+}
