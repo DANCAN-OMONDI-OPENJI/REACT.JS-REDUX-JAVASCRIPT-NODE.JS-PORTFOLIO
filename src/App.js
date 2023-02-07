a9ff5faf6d30aa97fe14013c61814d7ba80008a2
DANCAN OMONDI OPENJI <palmargenesis33@gmail.com>
2023-02-08 00:03:15 +0300

    Modified App.js Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 //import Counter from "./counter";
+import Header from "./Header";
 import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Blog from "./Blog";
+import Contact from "./Contact";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <Home />
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
