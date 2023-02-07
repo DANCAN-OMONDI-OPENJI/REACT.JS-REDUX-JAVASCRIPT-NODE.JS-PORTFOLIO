fa34edb093a4ce1f7ccec7267d22c82c2518befa
DANCAN OMONDI OPENJI <palmargenesis33@gmail.com>
2023-02-07 20:10:25 +0300

    Added Header.js

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <p>DANCAN OPENJI</p>
+      <ul className="header-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
