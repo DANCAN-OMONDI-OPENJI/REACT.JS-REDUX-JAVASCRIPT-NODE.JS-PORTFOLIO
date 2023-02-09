f0de1d20be10c674fb810c1b6fad714a4e52e4da
DANCAN OMONDI OPENJI <palmargenesis33@gmail.com>
2023-02-09 20:07:48 +0300

    Added about page contents

--- src/About.js
+++ src/About.js
@@ -3,14 +3,33 @@ import "./App.css";
 
 export default function About() {
   return (
-    <div>
-      <p>About Page</p>
-      <p>I and my wife Faith</p>
-      <img
-        src="./IMG-20220701-WA0043.jpg"
-        alt="Dancan Openji facing the camera"
-      />
+    <section className="about">
+      <p>
+        I am a junior front-end web developer with over 2years of experience in
+        building fast and interactive user interfaces with React and Redux. I am
+        a camper at freeCodeCamp where I have gained skills and completed many
+        projects.
+      </p>
+      <p>
+        I love JavaScript because it is a dynamic scripting language which adds
+        interactivity to my applications.
+      </p>
+      <div className="about-skills">
+        <h3>skills</h3>
+        <i className="ri-home-4-line"></i>
+        <i className="ri-heart-3-fill fill-color heart"></i>
+      </div>
+      <div className="about-education">
+        <h3>Education</h3>
+      </div>
+      <div className="img-div">
+        <p>I and my wife Faith</p>
+        <img
+          src="./IMG-20220701-WA0043.jpg"
+          alt="Dancan Openji facing the camera"
+        />
+      </div>
       <p>Home Page</p>
-    </div>
+    </section>
   );
 }
