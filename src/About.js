cccfb0f1d77909740afa2d478f15f65ec18f5866
DANCAN OMONDI OPENJI <palmargenesis33@gmail.com>
2023-04-12 12:29:11 +0300

    Modified About.js and Projects.js

--- src/About.js
+++ src/About.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
 export default function About() {
+  const navigate = useNavigate();
   return (
     <section className="about">
       <p>
@@ -16,8 +18,120 @@ export default function About() {
       </p>
       <div className="about-skills">
         <h3>skills</h3>
-        <i className="ri-home-4-line"></i>
-        <i className="ri-heart-3-fill fill-color heart"></i>
+        {
+          //<i className="ri-home-4-line"></i>
+          //<i className="ri-heart-3-fill fill-color heart"></i>
+        }
+        <div className="about-skills-img">
+          <img
+            src="https://www.datocms-assets.com/45470/1631110818-logo-react-js.png"
+            alt="react-logo"
+            width="50"
+            height="25"
+          ></img>
+          <img
+            src="https://cdn.zapier.com/storage/blog/4ec8fc7dc3a75758a3913bab9e5a4fd8_2.500x278.png"
+            alt="redux-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png"
+            alt="html-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png"
+            alt="css-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://sass-lang.com/assets/img/styleguide/seal-color-aef0354c.png"
+            alt="sass-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png"
+            alt="bootstrap-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://w7.pngwing.com/pngs/720/46/png-transparent-jquery-plain-wordmark-logo-icon-thumbnail.png"
+            alt="jquery-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon-thumbnail.png"
+            alt="figma-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://assets.stickpng.com/images/62a765c8bd73a4af5c5d4fbc.png"
+            alt="jest-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://d2nir1j4sou8ez.cloudfront.net/wp-content/uploads/2021/12/nextjs-boilerplate-logo.png"
+            alt="next.js-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://mui.com/static/logo.png"
+            alt="material-ui-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png"
+            alt="git-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://w7.pngwing.com/pngs/914/758/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png"
+            alt="github-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png"
+            alt="tailwind-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/919/919832.png"
+            alt="typescript-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://d3r49iyjzglexf.cloudfront.net/circleci-logo-stacked-fb-657e221fda1646a7e652c09c9fbfb2b0feb5d710089bb4d8e8c759d37a832694.png"
+            alt="circleci-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://w7.pngwing.com/pngs/640/199/png-transparent-javascript-logo-html-javascript-logo-angle-text-rectangle-thumbnail.png"
+            alt="javascript-logo"
+            width="50"
+            height="25"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png"
+            alt="javascript-logo"
+            width="50"
+            height="25"
+          />
+        </div>
       </div>
       <div className="about-education">
         <h3>Education</h3>
@@ -29,7 +143,8 @@ export default function About() {
           alt="Dancan Openji facing the camera"
         />
       </div>
-      <p>Home Page</p>
+
+      <button onClick={() => navigate("/")}>Home page</button>
     </section>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -2,12 +2,134 @@ import React from "react";
 
 export default function Projects() {
   return (
-    <div className="projects">
+    <section className="projects">
       <p>Projects Page</p>
       <p>
         Having gained all the skills required for front-end engineering, here
         are some of my projects to showcase my qualifications.
       </p>
-    </div>
+      <div className="project-links">
+        <div class="project-tile">
+          <a
+            href="https://github.com/DANCAN-OMONDI-OPENJI/React-Capstone-E-Comerce-Project"
+            target="blank"
+            rel="noreferrer"
+          >
+            <img
+              src=".\picsuit-store-screenshot.png"
+              alt="picsuit-store-link"
+              width="400"
+              height="200"
+            />
+            <p>
+              <span>&lt;</span>Picsuit Ecommerce Store<span>&#47;&gt;</span>
+              <span style={{ margin: "4px" }}>
+                <a
+                  href="https://cosmic-kelpie-393f30.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Preview Live
+                </a>
+              </span>
+            </p>
+          </a>
+        </div>
+        <div class="project-tile">
+          <a href="https://codepen.io/0PENJI/pen/NWBENLY?editors=1111">
+            <img
+              src="https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png"
+              alt="random-quote-machine-link"
+              width="400"
+              height="200"
+            />
+            <p>
+              <span>&lt;</span>
+              Random Quote Machine<span>&#47;&gt;</span>
+            </p>
+          </a>
+        </div>
+        <div class="project-tile">
+          <a
+            href="https://codepen.io/0PENJI/pen/PodZbxY?editors=1111"
+            target="blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn.freecodecamp.org/testable-projects-fcc/images/calc.png"
+              alt="JavaScript-calculator-link"
+              width="400"
+              height="200"
+            />
+            <p>
+              <span>&lt;</span>Javascript Calculator<span>&#47;&gt;</span>
+            </p>
+          </a>
+        </div>
+        <div class="project-tile">
+          <a
+            href="https://codepen.io/0PENJI/pen/bGjOyrB?editors=1111"
+            target="blank"
+            rel="noreferrer"
+          >
+            <img
+              src=".\markdown-previewer-screenshot.png"
+              alt="markdown-previewer-link"
+              width="400"
+              height="200"
+            />
+            <p>
+              <span>&lt;</span>Markdown Previewer<span>&#47;&gt;</span>
+            </p>
+          </a>
+        </div>
+        <div class="project-tile">
+          <a
+            href="https://codepen.io/0PENJI/pen/oNPNxgK"
+            target="blank"
+            rel="noreferrer"
+          >
+            <img
+              src=".\drum-machine-screenshot.png"
+              alt="drum-machine-link"
+              width="400"
+              height="200"
+            />
+            <p>
+              <span>&lt;</span>Drum Machine<span>&#47;&gt;</span>
+            </p>
+          </a>
+        </div>
+        <div class="project-tile">
+          <a
+            href="https://codepen.io/0PENJI/pen/OJogLKb"
+            target="blank"
+            rel="noreferrer"
+          >
+            <img
+              src=".\clock-screenshot.png"
+              alt="25-+-5-link"
+              width="400"
+              height="200"
+            />
+            <p>
+              <span>&lt;</span>25 + 5 Clock<span>&#47;&gt;</span>
+            </p>
+          </a>
+        </div>
+      </div>
+      <div class="show-all">
+        <a
+          href="https://github.com/DANCAN-OMONDI-OPENJI?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>
+            Show all
+            <i className="ri-arrow-right-s-line"></i>
+          </p>
+        </a>
+      </div>
+    </section>
   );
 }
